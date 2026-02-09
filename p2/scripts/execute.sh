2f0b53863b6072a2f3336cbbe218db2b4c0ef67d
Ayman Reifoun <git.ayman.reifoun@gmail.com>
2026-02-09 23:30:50 +0100

    okkkk

--- p2/scripts/execute.sh
+++ p2/scripts/execute.sh
@@ -1,11 +1,11 @@
-kubectl apply -f app1/deployment-app1.yaml
-kubectl apply -f app1/service-app1.yaml
+kubectl apply -f ../confs/app1/deployment-app1.yaml
+kubectl apply -f ../confs/app1/service-app1.yaml
 
-kubectl apply -f app2/deployment-app2.yaml
-kubectl apply -f app2/service-app2.yaml
+kubectl apply -f ../confs/app2/deployment-app2.yaml
+kubectl apply -f ../confs/app2/service-app2.yaml
 
-kubectl apply -f app3/deployment-app3.yaml
-kubectl apply -f app3/service-app3.yaml
+kubectl apply -f ../confs/app3/deployment-app3.yaml
+kubectl apply -f ../confs/app3/service-app3.yaml
 
-kubectl apply -f ingress.yaml
+kubectl apply -f ../confs/ingress.yaml
 
